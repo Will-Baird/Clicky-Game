55924e6a9161c0b78c252f18d0f016c646be80a1
Will Baird <whbaird21@gmail.com>
2018-10-31 16:28:44 -0500

    Updated

--- src/App.js
+++ src/App.js
@@ -3,25 +3,19 @@ import character from "./characters.json";
 import "./App.css";
 import Card from "./components/Card/Card.js";
 import Wrapper from "./components/Wrapper/Wrapper.js";
-
 class App extends React.Component {
-
     state = {
         guessed: [],
         score: 0,
         highScore: 0
     }
-
     checkValue = (event) => {
         event.preventDefault();
-
         let guess = event.target.dataset.value;
         let guessed = this.state.guessed
-
-        // Create different if statements
         for (let i = 0; i <= guessed.length; i++) {
             if (guess === guessed[i]) {
-                console.log("You Lose");
+                console.log("Incorrect, Try Again?");
                 console.log(this.state.highScore)
                 if (this.state.score >= this.state.highScore) {
                     let x = this.state.score;
@@ -36,7 +30,7 @@ class App extends React.Component {
                         highScore: this.state.highScore
                     };
                     this.setState(newState)
-                } // else if
+                }
                 return;
             } else {
                 if (i === guessed.length && guessed.length <= 11) {
@@ -52,49 +46,38 @@ class App extends React.Component {
                         }); 
                     }
                     return;
-                } //if
-            } // else
-        } // for
-    } // checkValue()
-
+                }
+            }
+        }
+    }
     random = () => {
-
-        let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+        let array = [0, 1, 2, 3, 4, 5, 6, 7, 8]
         let cards = []; 
-        
         const display = () => {
-    
             for (let i = 0; i < array.length; i++) {
                 cards.push(<Card name={character[array[i]].name} image={character[array[i]].image} value={character[array[i]].id} checkValue={this.checkValue} />)
-            }; // for
-
+            };
             return;
-        }; // display()
-
+        };
         const getNumbers = () => {
-
             for (let i = array.length - 1; i > 0; i--) {
                 const j = Math.floor(Math.random() * (i + 1));
-                [array[i], array[j]] = [array[j], array[i]]; // eslint-disable-line no-param-reassign
+                [array[i], array[j]] = [array[j], array[i]];
             }
-
             display();
-        }; // getNumbers()
-
+        };
         getNumbers();
         return cards;
-    } // random()
-
+    }
     render() {
         return (
             <div>
-            <header><h1 className="title">Clicky Game, Family Guy Edition!</h1> <h2 className="score">Score: <span className="actual">{this.state.score}</span> - High Score: <span className="high">{this.state.highScore}</span></h2></header>
+            <header><h1 className="title">Clicky Game</h1> <h2 className="score">Score: <span className="actual">{this.state.score}</span> - High Score: <span className="high">{this.state.highScore}</span></h2></header>
             <Wrapper>
                 {this.random()}
             </Wrapper>
             </div>
-        ) // return
-    }; // render
-} // class App
-
+        )
+    };
+}
 export default App;
